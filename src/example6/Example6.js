2e812c1c3d056a6065e8a488812f51076c30dd98
frontendgod <3054750147@qq.com>
2020-01-29 14:11:26 +0800

    p07

---
+++ src/example6/Example6.js
@@ -0,0 +1,16 @@
+import React, { useReducer } from 'react'
+import ShowArea from './showArea'
+import Buttons from './Buttons'
+import { Color } from './color'
+
+function Example6() {
+    return (
+        <div>
+            <Color>
+                <ShowArea></ShowArea>
+                <Buttons></Buttons>
+            </Color>
+        </div>
+    )
+}
+export default Example6
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import Example2 from './Example2';
 import Example3 from './Example3';
 import Example4 from './Example4';
 import Example5 from './Example5';
+import Example6 from './example6/Example6';
 
 
-ReactDOM.render(<Example5 />, document.getElementById('root'));
+ReactDOM.render(<Example6 />, document.getElementById('root'));
 
